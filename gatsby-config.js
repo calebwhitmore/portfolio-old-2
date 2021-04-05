d551fb2e62b5aeec31126cdf519f911a561f8d50
Caleb Whitmore <caleb.whitmore93@gmail.com>
2021-04-05 11:24:33 -0700

    trying to fix icon issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,11 +57,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `caleb-whitmores-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
-        display: `minimal-ui`, // This path is relative to the root of the site.
-        icon: `/src/images/logo.png`
+        display: `minimal-ui`,
+        icon: `src/images/cw-logo.png`
       },
     },
     {
